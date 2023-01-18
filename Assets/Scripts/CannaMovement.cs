bf86debd3a5e8ed660c61dd6e2a3fc83199b5f14
AlonsoRial <alonsorivasalcobendas@gmail.com>
2023-01-18 12:35:35 +0100

    movimiento y mapa

--- Assets/Scripts/CannaMovement.cs
+++ Assets/Scripts/CannaMovement.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CannaMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float Speed;
+
+    private Rigidbody2D Rigidbody2D;
+
+    private float Horizontal;
+
+    
     void Start()
     {
-        
+        Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Horizontal < 0.0f)
+        {
+            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        }//si se pulsa a la derecha, cambia a su escala a la positiva
+        else if (Horizontal > 0.0f)
+        {
+            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
+
+
+
+
     }
+
+
+    private void FixedUpdate()
+    {
+        //mover el personaje a la velocidad recogida por el Horizontal (teclado) hacia la dirección marcadaç
+        //la variable Speed se refiere a la velocidad, cuanto más alta, mas rapido se movera 
+        Rigidbody2D.velocity = new Vector2(Horizontal * Speed, Rigidbody2D.velocity.y);
+
+    }
+
+
+
 }
