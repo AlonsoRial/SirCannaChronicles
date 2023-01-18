e8f7cb6515ca6b77066543d5814eb794ff16fdab
AlonsoRial <alonsorivasalcobendas@gmail.com>
2023-01-18 12:40:45 +0100

    movimiento Camera

---
+++ Assets/Scripts/CameraScripts.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScripts : MonoBehaviour
+{
+
+    public GameObject Canna;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Canna != null)
+        {
+            //basicamente, el script copia la posición del personaje y se lo aplica a la camara
+            Vector3 position = transform.position;
+            position.x = Canna.transform.position.x;
+            transform.position = position;
+        }
+    }
+}
