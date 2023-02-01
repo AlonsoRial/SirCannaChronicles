2b980188640f9301e0735debb2b378802580a5a7
amsamu <samaropulido@gmail.com>
2023-02-01 17:46:32 +0100

    Proto menu inicial y pausa

--- Assets/Scripts/CameraScripts.cs
+++ Assets/Scripts/CameraScripts.cs
@@ -10,7 +10,7 @@ public class CameraScripts : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
